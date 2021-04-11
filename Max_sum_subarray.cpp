562e104649ea0a7699114e11a152e0ca9249884a
mr-robot <mahendrarathore1742@gmail.com>
2021-04-11 11:56:14 +0530

    add code

---
+++ Max_sum_subarray.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace  std;
+
+void subarray(int arr[],int n,int k,int x){
+
+		int sum=0,ans=0;
+
+		for (int i = 0; i <k ; i++)
+		{
+			sum+=arr[i];
+		}
+
+		if(sum<x){
+			ans=sum;
+		}
+
+		for (int i = k; i <n ; ++i)
+		{
+			sum=sum- arr[i-k];
+			sum=sum+arr[i];
+
+			if(sum<x){
+				ans=max(ans,sum);
+			}
+		}
+		cout<<ans<<endl;
+}
+
+int main(){
+
+	int arr[]={7,5,4,6,8,9};
+	int k=3;
+	int x=50;
+	int n=6;
+
+	subarray(arr,n,k,x);
+
+	return 0;
+}
--- Sliding_Window_Maximum.cpp
+++ Sliding_Window_Maximum.cpp
@@ -25,7 +25,7 @@ int main()
 {
 	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	int n = sizeof(arr) / sizeof(arr[0]);
-	int k = 3;
+	int k = 7;
 	printKMax(arr, n, k);
 	return 0;
 }
